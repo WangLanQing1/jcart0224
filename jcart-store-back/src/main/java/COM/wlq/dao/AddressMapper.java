2577a25572a24698d9256c9ad74537bcbaa82c3f
WlqNeverGiceUp <2257657950@qq.com>
2020-03-02 15:16:18 +0800

    jcart-store-back dao mapper

---
+++ jcart-store-back/src/main/java/COM/wlq/dao/AddressMapper.java
@@ -0,0 +1,17 @@
+package com.wlq.dao;
+
+import com.wlq.po.Address;
+
+public interface AddressMapper {
+    int deleteByPrimaryKey(Integer addressId);
+
+    int insert(Address record);
+
+    int insertSelective(Address record);
+
+    Address selectByPrimaryKey(Integer addressId);
+
+    int updateByPrimaryKeySelective(Address record);
+
+    int updateByPrimaryKey(Address record);
+}
