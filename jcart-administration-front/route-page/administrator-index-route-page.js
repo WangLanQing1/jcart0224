9adb49f48e45e35fed093a43eac7e35a007d4bf9
WlqNeverGiceUp <2257657950@qq.com>
2020-03-15 20:55:26 +0800

    fix page

--- jcart-administration-front/route-page/administrator-index-route-page.js
+++ jcart-administration-front/route-page/administrator-index-route-page.js
@@ -2,6 +2,7 @@ const AdministratorIndexRoutePage={
     template: `
     <div id="app">
         <el-button type="danger" @click="handleBatchDeleteClick">批量删除</el-button>
+        <el-button type="primary" @click="handleCreateClick">添加</el-button>
         <el-table :data="pageInfo.list" style="width: 100px" @selection-change="handleSelectionChange">
             <el-table-column type="selection" width="55">
             </el-table-column>
@@ -24,6 +25,7 @@ const AdministratorIndexRoutePage={
             <el-table-column label="操作">
                 <template slot-scope="scope">
                     <el-button size="mini" type="danger" @click="handleDelete(scope.$index,scope.now)">删除</el-button>
+                    <el-button size="mini" type="primary" @click="handleEdit(scope.$index,scope.now)">修改</el-button>
                 </template>
             </el-table-column>
         </el-table>
@@ -50,10 +52,17 @@ const AdministratorIndexRoutePage={
         this.getAdministrators();
     },
     methods: {
+        handleEdit(index,row){
+            this.$router.push('/administrator/update'+row.administratorId);
+        },
         handlePageChange(val){
             console.log('page change',val);
             this.pageNum = val;
             this.getAdministrators();
+        },
+        handleCreateClick(){
+            console.log('create click');
+            this.$router.push('/administrator/create');
         },
         handleDelete(index,row){
             console.log('delete click');
